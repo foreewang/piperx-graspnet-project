93f616d85bd760bbfb51fcb970a0f8cf0dbaa69f
foreewang <1300317526@qq.com>
2026-07-03 14:43:32 +0800

    封装运动规划接口并导出核心数据类型

---
+++ src/piperx_grasp/planning/__init__.py
@@ -0,0 +1,15 @@
+from .moveit_contract import (
+    MotionPlanRequest,
+    MotionPlanResult,
+    MotionWaypoint,
+    Pose6D,
+    pick_plan_to_motion_request,
+)
+
+__all__ = [
+    "MotionPlanRequest",
+    "MotionPlanResult",
+    "MotionWaypoint",
+    "Pose6D",
+    "pick_plan_to_motion_request",
+]
